33ce3cf1ffaa43365ece1253dc337bfe08b9abc4
Luis Felipe Claro <luisfelipe.claro@outlook.com>
2026-04-13 10:38:46 -0300

    telescope, snacks, nvimtree, blickcmp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,17 @@ vim.g.maplocalleader = ' '
 
 local mapkey = vim.keymap
 
-mapkey.set('n', '<leader>e', '<cmd>Ex<CR>', { desc = 'Open file tree' })
 mapkey.set('n', '<leader>sv', '<cmd>vsplit<CR>', { desc = 'Split vertical' })
 mapkey.set('n', '<leader>sh', '<cmd>split<CR>', { desc = 'Split horizontal' })
 mapkey.set('n', '<leader>q', '<cmd>q<CR>', { desc = 'Close buffer' })
 mapkey.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Open Lazy' })
+
+-- Better window navigation
+mapkey.set('n', '<C-h>', '<C-w>h', { desc = 'Focus left' })
+mapkey.set('n', '<C-j>', '<C-w>j', { desc = 'Focus down' })
+mapkey.set('n', '<C-k>', '<C-w>k', { desc = 'Focus up' })
+mapkey.set('n', '<C-l>', '<C-w>l', { desc = 'Focus right' })
+
+-- Escape insert mode
+mapkey.set('i', 'jk', '<Esc>')
+
